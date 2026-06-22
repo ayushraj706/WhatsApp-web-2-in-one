2cfc58bc382cfb4bb1c950c7866730ec29692b88
ayushraj706 <236733393+ayushraj706@users.noreply.github.com>
2026-06-22 22:59:28 +0530

    Add DATABASE_URL to PrismaClient configuration
    Add datasourceUrl to PrismaClient initialization.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,6 +6,7 @@ export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ["warn", "error"],
+    datasourceUrl: process.env.DATABASE_URL, // <-- Prisma 7 ke liye yahan URL add kiya gaya hai
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
