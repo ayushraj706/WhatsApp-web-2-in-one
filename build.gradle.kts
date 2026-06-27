d92ddc2898ade4b3dbc23094109da6b7842e5687
ayushraj706 <236733393+ayushraj706@users.noreply.github.com>
2026-06-27 09:24:30 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,3 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
