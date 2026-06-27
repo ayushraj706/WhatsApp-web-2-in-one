4096c0b3272b842b8f2d96d42f30989c47e605e4
ayushraj706 <236733393+ayushraj706@users.noreply.github.com>
2026-06-27 10:02:45 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,11 @@ android {
 }
 
 dependencies {
+   // ... baaki dependencies
+    implementation("jp.wasabeef:glide-transformations:4.3.0")
+    // agar Glide use kar rahe ho toh ye check kar lena:
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
@@ -65,7 +70,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
